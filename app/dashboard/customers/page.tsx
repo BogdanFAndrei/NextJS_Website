6846e2ac35c2400924ae03c3d1e9f6c42a802ce5
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-08 19:17:40 +0000

    TITLE EVERYTHING WORKING - Admin auth Done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,33 @@
-export default function CustomersPage() {
-    return <p>Customers Page</p>;
-  }
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
+}
 
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,6 +12,8 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 // Database connection
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -124,5 +126,24 @@ export async function deleteInvoice(id: string) {
   
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
 
