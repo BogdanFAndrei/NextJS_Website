198a989b99d7348d917566a1ba9a8bb80add2390
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-10 21:02:24 +0000

    feat: update invoice edit form to use GBP currency

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -78,7 +78,7 @@ export default function EditInvoiceForm({
                 type="number"
                 step="0.01"
                 defaultValue={invoice.amount}
-                placeholder="Enter USD amount"
+                placeholder="Enter GBP amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
               />
